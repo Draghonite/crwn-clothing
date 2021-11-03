13705ae588a1f9c00390d5004bd94241a4ff8110
Ivan Mercedes <ivan.mercedes@hybriddatasecurity.com>
2021-11-02 21:16:02 -0400

    add redux and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { auth, createUserProfileDocument} from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const additionalData = {
@@ -25,10 +21,10 @@ class App extends React.Component {
         };
         const userRef = await createUserProfileDocument(userAuth, additionalData);
         userRef.onSnapshot(snapshot => {
-          this.setState({ currentUser: { id: snapshot.id, ...snapshot.data() }});
+          setCurrentUser({ id: snapshot.id, ...snapshot.data() });
         })
       } else {
-        this.setState({ currentUser: null });
+        setCurrentUser(userAuth);
       }
     })
   }
@@ -40,7 +36,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -49,6 +45,10 @@ class App extends React.Component {
       </div>
     );
   }
-}
+};
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
